02271ae79b921c15a3f50f396bdbd19ba31c49a3
Stephen Wool <stevewool7@gmail.com>
2019-03-04 16:21:45 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -9,9 +9,9 @@ function forLoop(array) {
   return array
 }
 
-function whileLoop(number) {
-  while (number != 0) {
-    console.log(number);
-    number--
+function whileLoop(n) {
+  while (n != 0) {
+    console.log(n);
+    n--
   }
 }
